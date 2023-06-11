2a541c78108a7598249739a7328214cffecf3260
Thomas Heinis <thomas.heinis@ik.me>
2023-06-11 14:02:01 +0200

    Initial Commit

---
+++ SkyMate/SkyMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyMateApp.swift
+//  SkyMate
+//
+//  Created by Thomas Heinis on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SkyMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
