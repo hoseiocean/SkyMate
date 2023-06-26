440a134b7eb872db066018a28bba10a8b497c24b
Thomas Heinis <thomas.heinis@ik.me>
2023-06-26 20:41:29 +0200

    String extensions

---
+++ SkyMate/Utilities/Extensions/String+Normalization.swift
@@ -0,0 +1,25 @@
+//
+//  String+Normalization.swift
+//  SkyMate
+//
+//  Created by Thomas Heinis on 11/06/2023.
+//
+
+extension String {
+
+  /// A normalized version of the string.
+  ///
+  /// The normalized version of the string is computed by:
+  ///   - Replacing any diacritic marks with their non-diacritic equivalents.
+  ///   - Lowercasing the string.
+  ///   - Replacing all spaces with nothing.
+  ///   - Trimming white spaces and new lines at the start and end of the string.
+  var normalized: String {
+    self
+      .folding(options: .diacriticInsensitive, locale: .current)
+      .lowercased()
+      .replacingOccurrences(of: Constant.Character.space, with: Constant.Character.nothing)
+      .trimmingCharacters(in: .whitespacesAndNewlines)
+  }
+  
+}
---
+++ SkyMate/Utilities/Extensions/StringDictionary+KeyMatching.swift
@@ -0,0 +1,42 @@
+//
+//  StringDictionary+KeyMatching.swift
+//  SkyMate
+//
+//  Created by Thomas Heinis on 15/06/2023.
+//
+
+import Foundation
+
+/// This extension adds a method to Dictionary where the Key and Value are Strings.
+/// The method helps find a key or a prefix in the dictionary.
+extension Dictionary<String, String> {
+
+  /// This method is used to find a key or its prefix in the dictionary.
+  /// - Parameters:
+  ///   - searchString: The string to be searched in the keys.
+  ///   - normalize: A boolean to determine if the search is case sensitive. Default is true.
+  /// - Returns: A tuple with the found string and a boolean indicating if the match is a prefix.
+  func searchForKeyOrPrefix(like searchString: String, normalize: Bool = true) -> (value: String?, foundPrefix: Bool) {
+    let normalizedSearchString = normalize ? searchString.normalized : searchString
+
+    var foundPrefix = false
+
+    let matchingKey = keys.first(where: { key in
+      let normalizedKey = normalize ? key.normalized : key
+
+      if normalizedKey.hasPrefix(normalizedSearchString) {
+        foundPrefix = true
+        return normalizedKey == normalizedSearchString
+      }
+
+      return false
+    })
+
+    if let matchingKey {
+      return (self[matchingKey], foundPrefix)
+    }
+
+    return (nil, foundPrefix)
+  }
+
+}
