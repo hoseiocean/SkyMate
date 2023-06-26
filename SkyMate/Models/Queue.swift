61435d63ac03a0dfd71e40ed4bef7b891adddb10
Thomas Heinis <thomas.heinis@ik.me>
2023-06-26 21:44:36 +0200

    Queue implementation

--- SkyMate/Models/Queue.swift
+++ SkyMate/Models/Queue.swift
@@ -7,51 +7,46 @@
 
 import Foundation
 
-class Queue<T>: CustomStringConvertible {
+/// A thread-safe, fixed-capacity queue for storing items of any type.
+class Queue<T> {
 
   // MARK: - Properties
 
   private let capacity: Int
-  
-  let queueLock = DispatchQueue(label: String(describing: T.self) + "QueueLock")
+  private let queueLock = DispatchQueue(label: String(describing: T.self) + Constant.Name.queueLock)
 
+  /// The array storing the items of the queue.
   var queue: [T]
-
-  var description: String {
-    var result = "["
-    for item in queue {
-      result += "\(item), "
-    }
-    return result + "]"
-  }
 
   // MARK: - Initialization
 
-  /// Initializes a new SFTranscriptionSegmentBuffer with the specified capacity.
-  /// - Parameter capacity: The maximum number of segments the buffer can hold. Defaults to 3.
+  /// Initializes a new queue with the specified capacity.
+  /// - Parameter capacity: The maximum number of items the queue can hold. Defaults to 10.
+  ///   Must be greater than 0.
   init(capacity: Int = 10) {
+    assert(capacity > 0, "Capacity must be greater than 0")
     self.queue = [T]()
     self.capacity = max(.zero, capacity)
   }
 
   // MARK: - Public Methods
 
-  /// Removes all segments from the buffer.
+  /// Removes all items from the queue.
   func clear() {
     queueLock.sync {
       queue.removeAll()
     }
   }
 
-  /// Returns the number of segments currently in the buffer.
+  /// Returns the number of items currently in the queue.
   func count() -> Int {
     queueLock.sync {
       queue.count
     }
   }
 
-  /// Removes and returns the first segment from the buffer, or nil if the buffer is empty.
-  /// - Returns: The first SFTranscriptionSegment in the buffer, or nil if the buffer is empty.
+  /// Removes and returns the first item from the queue, or nil if the queue is empty.
+  /// - Returns: The first item in the queue, or nil if the queue is empty.
   @discardableResult func dequeue() -> T? {
     queueLock.sync {
       if !queue.isEmpty {
@@ -61,12 +56,9 @@ class Queue<T>: CustomStringConvertible {
     }
   }
 
-  func displaySegments() {
-    print(description)
-  }
-
-  /// Adds a segment to the buffer, removing the first segment if the buffer exceeds its capacity.
-  /// - Parameter segment: The SFTranscriptionSegment to enqueue.
+  /// Adds an item to the queue. If this causes the queue to exceed its capacity,
+  /// the first item in the queue is removed.
+  /// - Parameter item: The item to enqueue.
   func enqueue(_ segment: T) {
     queueLock.sync {
       queue.append(segment)
@@ -76,12 +68,20 @@ class Queue<T>: CustomStringConvertible {
     }
   }
 
-  /// Checks if the buffer is full (i.e., the number of segments equals the capacity).
-  /// - Returns: True if the buffer is full, false otherwise.
+  /// Executes a closure in a thread-safe manner, using the queue's lock.
+  /// - Parameter operation: The closure to execute. The closure's return value is returned by this method.
+  /// - Returns: The result of the `operation`.
+  func executeUnderLock<R>(_ operation: () -> R) -> R {
+    return queueLock.sync {
+      operation()
+    }
+  }
+
+  /// Checks if the queue is full (i.e., the number of items equals the capacity).
+  /// - Returns: True if the queue is full, false otherwise.
   func isFull() -> Bool {
     queueLock.sync {
       queue.count == capacity
     }
   }
-
 }
