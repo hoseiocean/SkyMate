0d0c6c656902a3cbdb2dadca95306b75f36b5c18
Thomas Heinis <thomas.Heinis@ik.me>
2023-08-03 21:58:36 +0200

    Documentation in CommandViewModel

--- SkyMate/ViewModels/CommandViewModel.swift
+++ SkyMate/ViewModels/CommandViewModel.swift
@@ -5,8 +5,15 @@
 //  Created by Thomas Heinis on 20/07/2023.
 //
 
+/// `CommandViewModel` is a ViewModel class for displaying the list of available commands.
 final class CommandViewModel {
-  
+
+  // MARK: - Initialization
+
+  /// Initializer for the `CommandViewModel` class.
+  ///
+  /// Upon initialization, it sets up a localized card displaying the list of commands and adds it
+  /// to the `CarrouselViewModel`.
   init() {
     let cardTitle = CommandText.title.localized
     let cardContent = CommandText.content.localized
