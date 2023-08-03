6592f050062bcf4b400dbf19aa2e1193307836ec
Thomas Heinis <thomas.Heinis@ik.me>
2023-08-03 12:53:49 +0200

    Adding documention in the Models - Card - Metar

--- SkyMate/Models/Card.swift
+++ SkyMate/Models/Card.swift
@@ -8,23 +8,42 @@
 import Combine
 import SwiftUI
 
+/// `Card` is a class that models a card with an `id`, a `title`, and a `content`.
 class Card: ObservableObject, Identifiable {
 
+  // MARK: - Properties
+
+  /// Unique identifier for `Card` instance.
   let id: UUID
 
+  /// The content of the `Card`.
   @Published var content: String
+
+  /// The title of the `Card`.
   @Published private(set) var title: String
 
+  // MARK: - Initialization
+
+  /// Initializes a new `Card` with the given `title` and `content`.
+  /// - Parameters:
+  ///   - title: The title of the `Card`.
+  ///   - content: The content of the `Card`.
   init(title: String, content: String) {
     self.id = UUID()
     self.title = title
     self.content = content
   }
 
+  // MARK: - Methods
+
+  /// Updates the `content` of the `Card`.
+  /// - Parameter newContent: The new content to update to.
   func updateContent(_ newContent: String) {
     self.content = newContent
   }
 
+  /// Updates the `title` of the `Card`.
+  /// - Parameter newTitle: The new title to update to.
   func updateTitle(_ newTitle: String) {
     self.title = newTitle
   }
--- SkyMate/Models/Metar.swift
+++ SkyMate/Models/Metar.swift
@@ -5,7 +5,14 @@
 //  Created by Thomas Heinis on 12/07/2023.
 //
 
+/// `Metar` is a structure that represents a METAR weather report.
 struct Metar: Codable {
+
+  // MARK: - Properties
+
+  /// An array of data strings, each representing a unique piece of METAR information.
   let data: [String]
+
+  /// The total number of METAR data results.
   let results: Int
 }
